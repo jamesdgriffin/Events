c9a0768592c6a2769ac777133e7e4ae23b608e5a
griffinjames1 <57371137+griffinjames1@users.noreply.github.com>
2021-03-11 15:53:05 -0500

    access ctrl in prog

--- events_app/lib/events_app_web/controllers/event_controller.ex
+++ events_app/lib/events_app_web/controllers/event_controller.ex
@@ -7,6 +7,63 @@ defmodule EventsAppWeb.EventController do
   alias EventsAppWeb.Plugs
   plug Plugs.RequireUser when action in [:new, :edit, :create, :update]
 
+  plug :fetch_event when action in [:show, :edit, :update, :delete]
+  plug :require_owner when action in [:edit, :update, :delete]
+
+  def require_owner(conn, _args) do
+    user = conn.assigns[:current_user]
+    event = conn.assigns[:event]
+
+    if user.id == event.user_id do
+      conn
+    else
+      conn
+      |> put_flash(:error, "That isn't your Event!")
+      |> redirect(to: Routes.page_path(conn, :index))
+      |> halt()
+    end
+  end
+
+  def fetch_event(conn, _args) do
+    id = conn.params["id"]
+    event = Events.get_event!(id)
+    assign(conn, :event, event)
+  end
+
+  def show(conn, %{"id" => _id}) do
+    event = conn.assigns[:event]
+    render(conn, "show.html", event: event)
+  end
+
+  def edit(conn, %{"id" => _id}) do
+    event = conn.assigns[:event]
+    changeset = Events.change_event(event)
+    render(conn, "edit.html", event: event, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => _id, "event" => event_params}) do
+    event = conn.assigns[:event]
+
+    case Events.update_event(event, event_params) do
+      {:ok, event} ->
+        conn
+        |> put_flash(:info, "Event updated successfully!")
+        |> redirect(to: Routes.event_path(conn, :show, event))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "edit.html", event: event, changeset: changeset)
+    end
+  end
+
+  def delete(conn, %{"id" => _id}) do
+    event = conn.assigns[:event]
+    {:ok, _event} = Events.delete_event(event)
+
+    conn
+    |> put_flash(:info, "Event deleted successfully!")
+    |> redirect(to: Routes.event_path(conn, :index))
+  end
+
   def index(conn, _params) do
     events = Events.list_events()
     render(conn, "index.html", events: events)
@@ -26,42 +83,42 @@ defmodule EventsAppWeb.EventController do
         |> put_flash(:info, "Event created successfully.")
         |> redirect(to: Routes.event_path(conn, :show, event))
 
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
-    end
-  end
+        {:error, %Ecto.Changeset{} = changeset} ->
+          render(conn, "new.html", changeset: changeset)
+        end
+      end
 
-  def show(conn, %{"id" => id}) do
-    event = Events.get_event!(id)
-    render(conn, "show.html", event: event)
-  end
+      def show(conn, %{"id" => id}) do
+        event = Events.get_event!(id)
+        render(conn, "show.html", event: event)
+      end
 
-  def edit(conn, %{"id" => id}) do
-    event = Events.get_event!(id)
-    changeset = Events.change_event(event)
-    render(conn, "edit.html", event: event, changeset: changeset)
-  end
-
-  def update(conn, %{"id" => id, "event" => event_params}) do
-    event = Events.get_event!(id)
-
-    case Events.update_event(event, event_params) do
-      {:ok, event} ->
-        conn
-        |> put_flash(:info, "Event updated successfully.")
-        |> redirect(to: Routes.event_path(conn, :show, event))
-
-      {:error, %Ecto.Changeset{} = changeset} ->
+      def edit(conn, %{"id" => id}) do
+        event = Events.get_event!(id)
+        changeset = Events.change_event(event)
         render(conn, "edit.html", event: event, changeset: changeset)
-    end
-  end
+      end
 
-  def delete(conn, %{"id" => id}) do
-    event = Events.get_event!(id)
-    {:ok, _event} = Events.delete_event(event)
+      def update(conn, %{"id" => id, "event" => event_params}) do
+        event = Events.get_event!(id)
 
-    conn
-    |> put_flash(:info, "Event deleted successfully.")
-    |> redirect(to: Routes.event_path(conn, :index))
-  end
-end
+        case Events.update_event(event, event_params) do
+          {:ok, event} ->
+            conn
+            |> put_flash(:info, "Event updated successfully.")
+            |> redirect(to: Routes.event_path(conn, :show, event))
+
+            {:error, %Ecto.Changeset{} = changeset} ->
+              render(conn, "edit.html", event: event, changeset: changeset)
+            end
+          end
+
+          def delete(conn, %{"id" => id}) do
+            event = Events.get_event!(id)
+            {:ok, _event} = Events.delete_event(event)
+
+            conn
+            |> put_flash(:info, "Event deleted successfully.")
+            |> redirect(to: Routes.event_path(conn, :index))
+          end
+        end
--- events_app/lib/events_app_web/helpers.ex
+++ events_app/lib/events_app_web/helpers.ex
@@ -2,4 +2,9 @@ defmodule EventsAppWeb.Helpers do
   def have_current_user?(conn) do
     conn.assigns[:current_user] != nil
   end
+
+  def current_user_id?(conn, user_id) do
+    user = conn.assigns[:current_user]
+    user && user.id == user_id
+  end
 end
